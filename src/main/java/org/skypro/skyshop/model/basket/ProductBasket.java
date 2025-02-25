2dd2038b523e93f197100122ecc477266279f618
Vitaliy-Avdeev <avd.vit78@gmail.com>
2025-02-25 11:11:43 +0400

    Implementation of the task - Life cycle of Spring Boot application components.

--- src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
+++ src/main/java/org/skypro/skyshop/model/basket/ProductBasket.java
@@ -2,9 +2,16 @@ package org.skypro.skyshop.model.basket;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
-public record ProductBasket(Map<UUID, Integer> basket) {
+@SessionScoped
+public final class ProductBasket {
+    private final Map<UUID, Integer> basket;
+
+    public ProductBasket(Map<UUID, Integer> basket) {
+        this.basket = basket;
+    }
 
     public void addProduct(UUID productId) {
         if (basket.containsKey(productId)) {
@@ -14,8 +21,27 @@ public record ProductBasket(Map<UUID, Integer> basket) {
         }
     }
 
-    @Override
     public Map<UUID, Integer> basket() {
         return Collections.unmodifiableMap(basket);
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (ProductBasket) obj;
+        return Objects.equals(this.basket, that.basket);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(basket);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductBasket[" +
+                "basket=" + basket + ']';
+    }
+
 }
